a8c79ce8cafd4dca325831382bccd2cfbeb1ceed
Ebenrich 29 <ebenrich29@gmail.com>
2025-08-03 22:54:41 +0000

    updated

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,13 +3,29 @@ import { create } from 'zustand';
 const useRecipeStore = create((set) => ({
   recipes: [],
   
+  // CRUD Actions
   addRecipe: (newRecipe) => 
-    set((state) => ({ 
-      recipes: [...state.recipes, newRecipe] 
-    })),
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
     
   setRecipes: (recipes) => 
     set({ recipes }),
+    
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map(recipe =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      )
+    })),
+    
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter(recipe => recipe.id !== id)
+    })),
+    
+  // Helper to find single recipe
+  getRecipeById: (id) => {
+    return useRecipeStore.getState().recipes.find(recipe => recipe.id === id);
+  }
 }));
 
 export default useRecipeStore;

0119822799b72aeac2e35a0d98099e249224b13a
Ebenrich 29 <ebenrich29@gmail.com>
2025-08-24 20:15:57 +0000

    Recipe App

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
